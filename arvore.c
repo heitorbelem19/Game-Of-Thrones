66e008d06a91d6e56a4a1c6482d27e82625cb3f8
heitorbelem19 <32557513+heitorbelem19@users.noreply.github.com>
2017-11-20 10:54:20 -0200

    Update arvore.c

--- arvore.c
+++ arvore.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include "arvore.h"
 
 Character* SalvarPersonagens(){
@@ -11,8 +12,8 @@ Character* SalvarPersonagens(){
 		exit(1);
 	}
 
-	char *nome = (char *)malloc(sizeof(char)*1000);
-	char *casa = (char *)malloc(sizeof(char)*1000);
+	char *nome = (char *)malloc(sizeof(char)*100);
+	char *casa = (char *)malloc(sizeof(char)*100);
 	int agilidade,forca,inteligencia,saude;
 	
 	Character *vetorPersonagens = (Character *)malloc(sizeof(Character)*20); /*cada elemento desse vetor é uma Struct do tipo Character, que possui os atributos dos personagens*/
@@ -65,8 +66,6 @@ Character* SalvarPersonagens(){
 	return vetorPersonagens;
 }
 
-
-
 Character* character_create(char* _name, char* _house, int _agility, int _strength, int _intelligence, int _health){
 	
 	Character *new_character = (Character *)malloc(sizeof(Character)); /*aloca memória para o personagem */
@@ -80,6 +79,77 @@ Character* character_create(char* _name, char* _house, int _agility, int _streng
 	new_character->health = _health; /*copia o atributo "Saude" para o novo personagem*/
 	
 	return new_character; /*retorna o endereço do personagem criado*/
+}
+
+t_node* tree_create(){
+
+	t_node *tree = (t_node*)malloc(sizeof(t_node));
+	tree->character = NULL;
+	tree->left = NULL;
+	tree->right = NULL;
+
+	return tree;
+}
+
+t_lista* CriaLista(){
+	
+	t_lista *lista = (t_lista *)malloc(sizeof(t_lista));
+	lista->inicio = NULL;
+	lista->fim = NULL;
+
+	return lista;
+}
+
+
+void InserirInicio(Character *vetor, t_lista* lista){
+	
+	t_elemento *new_element = (t_elemento *)malloc(sizeof(t_elemento));
+	new_element->proximo = NULL;
+	new_element->anterior = NULL;
+	new_element->character->name = vetor->name;
+	new_element->character->house = vetor->house;
+	new_element->character->agility = vetor->agility;
+	new_element->character->strength = vetor->strength;
+	new_element->character->intelligence = vetor->intelligence;
+	new_element->character->health = vetor->health;
+	 
+	if(lista->inicio == NULL){
+		lista->inicio = new_element;
+		lista->fim = new_element;
+	}
+	else{
+		lista->inicio->anterior = new_element;
+		new_element->proximo = lista->inicio;
+		lista->inicio = new_element;
+	}
+}
+
+t_lista* ListaPersonagem(Character *vetor){
+
+	t_lista *ListaCharacter = CriaLista();
+	int i,x;
+
+	for(i=0;i<16;i++){
+		srand((unsigned)time(NULL));
+		x = rand() % 16;
+		Character *novo;
+		novo = character_create(vetor[x].name, vetor[x].house, vetor[x].agility, vetor[x].strength, vetor[x].intelligence, vetor[x].health);
+		InserirInicio(novo, ListaCharacter);
+	}
+
+	return ListaCharacter;
+}
+
+void Printar_Lista(t_lista* lista){
+	t_elemento* ptr = lista->inicio;
+	if(lista->inicio == NULL){
+		printf("LISTA VAZIA!\n");
+		return;
+	}
+	while(ptr != NULL){
+		printf("%s, %s, %d, %d, %d, %d\n",ptr->character->name, ptr->character->house, ptr->character->agility, ptr->character->strength, ptr->character->intelligence, ptr->character->health);
+		ptr = ptr->proximo;
+	}
 }
 
 
