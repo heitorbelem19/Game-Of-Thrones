8ce9c61a1b23fcd48bc8b1cd0a8cc560c92f501a
heitorbelem19 <32557513+heitorbelem19@users.noreply.github.com>
2017-11-20 10:56:03 -0200

    Update main.c

--- main.c
+++ main.c
@@ -2,16 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <time.h>
 #include "arvore.h"
 
 int main(){
 
 	Character *Personagens = (Character *)malloc(sizeof(Character)*20);
 	Personagens = SalvarPersonagens();
+	t_lista *ListaCharacter = ListaPersonagem(Personagens);
+	Printar_Lista(ListaCharacter);
 
-    for(int i=0;i<20;i++){
+    /*for(int i=0;i<20;i++){
     	printf("%s, %s, %d, %d, %d, %d\n", Personagens[i].name, Personagens[i].house,Personagens[i].agility, Personagens[i].strength, Personagens[i].intelligence, Personagens[i].health);
-    }
+    }*/
 
     return 0;
 }
